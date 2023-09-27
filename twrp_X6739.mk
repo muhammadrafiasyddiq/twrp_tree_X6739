1ac1f17eb1f48b06bf0504ac303c3ab47ddfe3c6
muhammadrafiasyddiq <muhammadrafigaming8@gmail.com>
2023-09-27 21:08:02 +0700

    Initial Test

---
+++ twrp_X6739.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2023 The TWRP Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit from X6827 device
+$(call inherit-product, device/infinix/X6739/device.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := X6739
+PRODUCT_NAME := twrp_X6739
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix GT 10 Pro
+PRODUCT_MANUFACTURER := infinix
